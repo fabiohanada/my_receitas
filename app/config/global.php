cd9d0aa1cc4f691e1dc8a3818aed5e896ab0031b
fabiohanada23 <61828534+fabiohanada23@users.noreply.github.com>
2024-12-11 21:36:32 -0300

    first commit

---
+++ app/config/global.php
@@ -0,0 +1,10 @@
+<?php
+
+define('BASE', '/my_receitas/');
+define('UNSET_COUNT', 1);
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'id18832490_fabio');
+define('DB_PASS', 'Carrobom0509!');
+define('DB_NAME', 'id18832490_my_receitas');
+
---
+++ app/site/view/sobre/main.twig.php
@@ -0,0 +1,14 @@
+{% extends "partials/body.twig.php" %}
+
+{% block title %} Sobre - My Receitas {% endblock %}
+
+{% block body %}
+<h1>Sobre</h1>
+
+<div>
+
+    <p>O Minhas Receitas é o maior site de receitas do Brasil e, desde 2005, reúne uma comunidade de apaixonados por comida. A ideia era criar um site colaborativo, uma espécie de caderno de receitas compartilhado que reunisse os tradicionais biscoitos de família e as novas descobertas na cozinha. Hoje, mais de um milhão de brasileiros passa por aqui todos os dias e nossa missão é ajudar esses leitores a cozinhar com simplicidade e prazer. O blog do Minhas Receitas nasceu em 2016 com o objetivo de expandir nossas receitas em dicas e nos comunicar com a nossa audiência apaixonada pelo mundo da gastronomia. Por aqui você aprende desde o que fazer para um bolo não solar, até como deixar uma pia brilhando com dicas caseiras. Nós também adoramos desvendar curiosidades sobre a culinária de lugares do Brasil ou do mundo e somos experts em fazer seleções de receitas com os ingredientes que você tem em casa. Para nós, o que importa é te inspirar a botar a mão na massa na cozinha! </p>
+    <p>Conheça o time do Minhas Receitas</p>
+</div>
+
+{% endblock %}
